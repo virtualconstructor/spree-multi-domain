03b4545b45c8066a7515f36d23465a42cc89e981
rozner <mihaly.rozner@virtualconstructor.com>
2014-08-19 02:22:40 +0200

    compatibility with spree_auth
    strange error: Spree::Core::ControllerHelpers.included(base) is not called in UserRegistrationsController when the deleted line is present

--- lib/spree_multi_domain/engine.rb
+++ lib/spree_multi_domain/engine.rb
@@ -17,28 +17,7 @@ module SpreeMultiDomain
 
     config.to_prepare &method(:activate).to_proc
 
-    initializer "templates with dynamic layouts" do |app|
-      ActionView::TemplateRenderer.class_eval do
-        def find_layout_with_multi_store(layout, locals)
-          store_layout = layout
-
-          if @view.respond_to?(:current_store) && @view.current_store && !@view.controller.is_a?(Spree::Admin::BaseController)
-            store_layout = layout.call.gsub("layouts/", "layouts/#{@view.current_store.code}/")
-          end
-
-          begin
-            find_layout_without_multi_store(store_layout, locals)
-          rescue ::ActionView::MissingTemplate
-            find_layout_without_multi_store(layout, locals)
-          end
-        end
-
-        alias_method_chain :find_layout, :multi_store
-      end
-    end
-
     initializer "current order decoration" do |app|
-      require 'spree/core/controller_helpers/order'
       ::Spree::Core::ControllerHelpers::Order.module_eval do
         def current_order_with_multi_domain(create_order_if_necessary = false)
           current_order_without_multi_domain(create_order_if_necessary)
